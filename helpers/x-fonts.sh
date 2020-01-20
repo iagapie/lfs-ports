49db088f3aa4216f1ce22879fda5e0129fffd054
Igor Agapie <igoragapie@gmail.com>
2020-01-21 00:00:37 +0200

    start to add xorg ports

---
+++ helpers/x-fonts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CURDIR=$(cd "$(dirname $0)" && pwd)
+XDIR="$CURDIR/../xorg"
+
+mkdir -p $XDIR
+
+for p in $(grep -v '^#' $CURDIR/md5/x-font.md5 | awk '{print $2}'); do
+    n=$(echo $p | sed 's/^\(.*\)-\(.*\)/\1/')
+    nl=$(echo $n | awk '{print tolower($0)}')
+    v=$(echo $p | sed 's/^\(.*\)-\(.*\).tar.\(.*\)/\2/')
+    ar=$(echo $p | sed 's/^\(.*\)-\(.*\).tar.\(.*\)$/tar.\3/')
+
+    mkdir -p $XDIR/$nl
+
+    cat <<EOF > $XDIR/$nl/spkgbuild
+# description	: $nl
+# maintainer	: iagapie, igoragapie at gmail.com
+# depends	    : 
+
+name=$nl
+version=$v
+release=1
+source="https://www.x.org/pub/individual/font/$n-\$version.$ar"
+
+build() {
+	cd $n-\$version
+
+    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static
+    make
+	make DESTDIR=\$PKG install
+
+    rm -r \$PKG/usr/share/doc
+}
+EOF
+done
